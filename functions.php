77cd689021ae16112b863493717b6993f45f466f
Dustin Scherer <dustin@angrylaser.com>
2015-10-29 03:58:33 +0000

    advanced template to less + minify with bootstrap menu

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
+
+    include ( dirname(__FILE__) . "/bootstrap_functions.php" );
+
+    function theme_wp_hook() {
+        wp_enqueue_style('theme-bootstrap', get_template_directory_uri().'/libs/bootstrap/css/bootstrap.css', array(), false, 'screen,projection');
+        wp_enqueue_style('theme-bootstrap-responsive', get_template_directory_uri().'/libs/bootstrap/css/bootstrap-responsive.css', array(), false, 'screen,projection');
+        wp_enqueue_style('theme', get_template_directory_uri().'/style.less', array(), false, 'screen,projection');
+    }
+
     function wpbootstrap_scripts_with_jquery(){
         wp_register_script(
             'custom-script', 
@@ -8,4 +17,6 @@
         wp_enqueue_script( 'custom-script' );
     }
     add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery' );
+    add_action( 'wp_enqueue_scripts', 'theme_wp_hook');
+
 ?>
